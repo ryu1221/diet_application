38b4a6d4558474768c442a1c04fe215d16b0c5dc
ryu1221 <ryuchan.pool12210517@gmail.com>
2020-10-16 23:40:02 +0900

    add logout,login

--- app/app/controllers/sessions_controller.rb
+++ app/app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create 
+    @user = User.find_by(email: params[:email]);
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:notice] = "ログインに成功しました"
+      redirect_to("/ranks")
+    else
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      flash[:notice] = "ログインに失敗しました"
+      render("new")
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/ranks")
+  end
 end
--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_121838) do
+ActiveRecord::Schema.define(version: 2020_10_16_143139) do
 
   create_table "ranks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "rank"
@@ -28,9 +28,9 @@ ActiveRecord::Schema.define(version: 2020_10_16_121838) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
