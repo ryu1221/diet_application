9ca127cb03cc7350541bf3e328938440540f9806
ryu1221 <ryuchan.pool12210517@gmail.com>
2020-10-16 21:29:06 +0900

    create users_controller

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-  # resources :ranks, only:[:index]
 
   get '/:genre' => 'ranks#index'
   get '/:genre/:id' => 'ranks#show'
+
+  resources :users, only:[:new, :create]
+
+
 end
