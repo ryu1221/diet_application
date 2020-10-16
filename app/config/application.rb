82d0f9749a06e3709011ed2380437d61ff0adf1f
ryu1221 <ryuchan.pool12210517@gmail.com>
2020-10-16 19:35:26 +0900

    add pagenation

--- app/config/application.rb
+++ app/config/application.rb
@@ -16,5 +16,6 @@ module App
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.hosts << "localhost"
+    config.i18n.default_locale = :ja
   end
 end
