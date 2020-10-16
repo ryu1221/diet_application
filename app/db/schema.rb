fe8a850968e97687fde80db62693c8ae8922d7e7
ryu1221 <ryuchan.pool12210517@gmail.com>
2020-10-16 21:20:43 +0900

    create user_model

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_070356) do
+ActiveRecord::Schema.define(version: 2020_10_16_121838) do
 
   create_table "ranks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "rank"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2020_10_16_070356) do
     t.text "itemCaption"
     t.string "itemUrl"
     t.string "genre"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
