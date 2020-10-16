ca8c7761f520883a5bbc56ef04bde7591a333c77
ryu1221 <ryuchan.pool12210517@gmail.com>
2020-10-16 22:12:00 +0900

    add validation

--- app/app/models/user.rb
+++ app/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-
   has_many :ranks
+
+  validates :name , {presence: true}
+  validates :email, {uniqueness: true, presence: true}
+  validates :password, {length: {minimum: 8}}
 end
