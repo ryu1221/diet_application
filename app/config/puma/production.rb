1644309490460803c8b1b02948bb201edbf27d82
ryu1221 <ryuchan.pool12210517@gmail.com>
2020-10-18 21:46:06 +0900

    modified production.rb

--- app/config/puma/production.rb
+++ app/config/puma/production.rb
@@ -6,3 +6,20 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
 plugin :tmp_restart
+
+
+before_fork do
+  PumaWorkerKiller.config do |config|
+    config.ram           = 1024 
+    config.frequency     = 5 * 60 
+    config.percent_usage = 0.9 
+    
+    config.rolling_restart_frequency = 24 * 3600 
+    config.reaper_status_logs = true
+  end
+  PumaWorkerKiller.start
+  ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
+end
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
