309e57fec94aba0ff4b8de12d18b8a723e4790cd
ryu1221 <ryuchan.pool12210517@gmail.com>
2020-10-18 20:59:41 +0900

    delete puma.pids

--- app/config/puma/production.rb
+++ app/config/puma/production.rb
@@ -2,7 +2,6 @@ environment ENV.fetch("RAILS_ENV") { "production" }
 
 app_root = File.expand_path("../..", __FILE__)
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
-pidfile "#{app_root}/tmp/pids/puma.pid"
 
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
