d8c2d67590b1849ba3dc17574c890cfe67ef196b
ryu1221 <ryuchan.pool12210517@gmail.com>
2020-10-18 15:08:19 +0900

    add puma/production.rb

---
+++ app/config/puma/production.rb
@@ -0,0 +1,13 @@
+environment ENV.fetch("RAILS_ENV") { "production" }
+
+app_root = File.expand_path("../..", __FILE__)
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
+pidfile "#{app_root}/tmp/pids/puma.pid"
+
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+threads threads_count, threads_count
+workers 2 
+preload_app!
+
+daemonize
+stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
