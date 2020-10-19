c05598a2d3ad30892753501dbb2b8b8d18af2b78
ryu1221 <ryuchan.pool12210517@gmail.com>
2020-10-19 20:17:31 +0900

    modified docker-compose.prod.yml

--- app/config/puma/production.rb
+++ app/config/puma/production.rb
@@ -9,6 +9,7 @@ plugin :tmp_restart
 bind "unix:///app/tmp/sockets/puma.sock"
 
 daemonize
+pidfile "/app/pids/puma.pid"
 stdout_redirect "/app/log/puma.stdout.log", "/app/log/puma.stderr.log", true
 
 
