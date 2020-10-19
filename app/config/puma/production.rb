dca6e0f64b1497e98988d1b9b847c21b808d17d7
ryu1221 <ryuchan.pool12210517@gmail.com>
2020-10-19 15:58:28 +0900

    modified production.rb

--- app/config/puma/production.rb
+++ app/config/puma/production.rb
@@ -7,7 +7,7 @@ environment ENV.fetch("RAILS_ENV") { "production" }
 plugin :tmp_restart
 
 app_root = File.expand_path("../..", __FILE__)
-bind "unix://#{app_root}/tmp/sockets/puma.sock"
+bind "unix:///app/tmp/sockets/puma.sock"
 
 daemonize true
 stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
