5a0436a40cbfd2e2c6aaf4a1048693b67f8d8439
ryu1221 <ryuchan.pool12210517@gmail.com>
2020-10-19 13:38:28 +0900

    modified production.rb

--- app/config/puma/production.rb
+++ app/config/puma/production.rb
@@ -4,10 +4,10 @@ threads threads_count, threads_count
 environment ENV.fetch("RAILS_ENV") { "production" }
 plugin :tmp_restart
 
-# app_root = File.expand_path("../..", __FILE__)
-bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+app_root = File.expand_path("../..", __FILE__)
+bind "unix:///#{app_root}/tmp/sockets/puma.sock"
 
-stdout_redirect "#{Rails.root}/log/puma.stdout.log", "#{Rails.root}/log/puma.stderr.log", true
+stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
 
 
 
