9912c4a79302c52bc7ee5cc2004d9f74d050bdd9
ryu1221 <ryuchan.pool12210517@gmail.com>
2020-10-19 21:38:26 +0900

    modified production.rb

--- app/config/production.rb
+++ app/config/production.rb
@@ -1,5 +1,6 @@
 environment ENV.fetch("RAILS_ENV") { "production" }
-bind "unix:///app/tmp/sockets/puma.sock"
+app_root = File.expand_path("/..", __FILE__)
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 workers 2
@@ -7,7 +8,7 @@ preload_app!
 
 daemonize
 pidfile "/app/pids/puma.pid"
-stdout_redirect "/app/log/puma.stdout.log", "/app/log/puma.stderr.log", true
+stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
 
 plugin :tmp_restart
 
