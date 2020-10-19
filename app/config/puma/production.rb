4e49188fceb8f609f088d7a4ee535f0b47faf332
ryu1221 <ryuchan.pool12210517@gmail.com>
2020-10-19 20:27:15 +0900

    modified production.rb

--- app/config/puma/production.rb
+++ app/config/puma/production.rb
@@ -1,7 +1,7 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
-workers 2 
-preload_app!
+# workers 2 
+# preload_app!
 # port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "production" }
 plugin :tmp_restart
