4924c426b22dcca169932d52e26daa9a8c92a541
ryu1221 <ryuchan.pool12210517@gmail.com>
2020-10-22 15:17:42 +0900

    modified nginx.conf

--- app/config/initializers/mime_types.rb
+++ app/config/initializers/mime_types.rb
@@ -2,3 +2,8 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+Mime::Type.register "text/richtext", :rtf
+Mime::Type.register "text/css", :css
+Mime::Type.register "text/richtext", :rtf
+Mime::Type.register "text/richtext", :rtf
+Mime::Type.register "text/richtext", :rtf
