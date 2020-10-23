ce9b2d6b839b33924f665158f6bc4505da705558
ryu1221 <ryuchan.pool12210517@gmail.com>
2020-10-23 20:56:02 +0900

    modified routes.rb

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   root 'ranks#index'
-  resources :sessions, only:[:new, :create, :destroy]
-  resources :users, only:[:new, :create] do
+  resources :sessions, only: %i(new create destroy)
+  resources :users, only: %i(new create) do
     get :mypage
   end
-  resources :goods, only:[] do
+  resources :goods, only: %i() do
     get :search, on: :collection
     post :search_list, on: :collection
   end
-  resources :favorite, only: [:create, :destroy]
+  resources :favorite, only: %i(create destroy)
   
   get '/:genre' => 'ranks#index'
   get '/:genre/:id' => 'ranks#show'
